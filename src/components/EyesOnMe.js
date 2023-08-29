6cc4af240aa20d9088f985791e64c1b175746e1d
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-29 05:32:57 +0300

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    return (
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>
+            Eyes on me
+        </button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,10 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={() => console.log('Entering password...')} />
+        </div>
     )
 }
 
